affe9825e74f63352a6c6146298d553f3d5bf42e
genevieveanderson1 <genevieve.anderson1@marist.edu>
2021-10-06 18:46:07 -0400

    Lab5_2

--- Labs/Lab 5/Submission/script.js
+++ Labs/Lab 5/Submission/script.js
@@ -1,7 +1,8 @@
 document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('title')[0].innerHTML = 'change text';
 document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('p')[0].innerHTML = 'Genevieve Anderson';
-document.getElementById('bossy').innerHTML = 'Here are some things you could do if you know how!';
-document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].style.color = 'blue';
+document.getElementByTagName('bossy').innerHTML = 'Here are some things you could do if you know how!';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.color = 'blue';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.backgroundColor = #FFFDD0;
 document.body.style.backgroundColor = 'gray';
-document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].style.textTransform = 'uppercase';
-document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].innerHTML = 'SD2';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].style.textTransform = 'uppercase';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].getElementsByTagName('li')[0].innerHTML = 'SD2';

438fea462eebbe2d095807e95550d9d0780ac95e
genevieveanderson1 <genevieve.anderson1@marist.edu>
2021-10-06 17:16:01 -0400

    Lab5

---
+++ Labs/Lab 5/Submission/script.js
@@ -0,0 +1,7 @@
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('title')[0].innerHTML = 'change text';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('p')[0].innerHTML = 'Genevieve Anderson';
+document.getElementById('bossy').innerHTML = 'Here are some things you could do if you know how!';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].style.color = 'blue';
+document.body.style.backgroundColor = 'gray';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].style.textTransform = 'uppercase';
+document.getElementsByTagName('body')[0].getElementsByTagName('section')[0].getElementsByTagName('ul')[0].innerHTML = 'SD2';
